52be2a86be65c1b4db2ad64e628d2fb9a266d180
UENO27 <s1310027@u-aizu.ac.jp>
2024-07-04 15:01:39 +0900

    User name added

---
+++ user_name.java
@@ -0,0 +1,31 @@
+import java.util.Random;
+import java.util.Scanner;
+
+class user_name{
+    public static void main(String[] args){
+	Scanner sc = new Scanner(System.in);
+	int t = 0;
+	int f = 0;
+        Random r = new Random();
+	
+	System.out.println("Who are you?");
+	String Name = sc.next();
+	System.out.println("Hello, " + Name + "!");
+	System.out.println("Tossing a coin...");
+	for(int i = 1; i <= 3; i++){
+	    boolean b = r.nextBoolean();
+	    System.out.print("Round " + i +": ");
+	    if(b){
+		System.out.println("Hand");
+		t++;
+	    }
+	    else {
+		System.out.println("Tail");
+		f++;
+	    }
+	}
+	System.out.println("Heads: " + t +", Tails: "+ f);
+	if(t > f)System.out.println(Name + " won!");
+	if(t < f)System.out.println(Name + " lost.");
+    }
+}
